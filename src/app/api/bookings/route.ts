5e50b2312ab06cc7824bd53f500d767751b6fa48
Rebeka Geczi <geczirebeka@gmail.com>
2025-11-27 15:43:50 +0000

    Add route for getting current bookings from supabase

---
+++ src/app/api/bookings/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+import { createServerClient } from "@/lib/supabase/server";
+
+export async function GET(req: { url: string | URL }) {
+  const { searchParams } = new URL(req.url);
+  const clubId = searchParams.get("clubId");
+  const date = searchParams.get("date");
+
+  const supabase = createServerClient();
+
+  const { data } = await supabase
+    .from("bookings")
+    .select("*")
+    .eq("club_id", clubId)
+    .eq("date", date)
+    .neq("status", "cancelled");
+
+  return NextResponse.json(data ?? []);
+}
